8a35e52a3a85dc47fb77089391d6832ed7d6b706
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-04-28 09:38:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+if (pickupLocation <= 42) {
+    return 42 - pickupLocation;
+}
+else if (pickupLocation >= 42) {
+    return pickupLocation - 42;
+}
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+   
+      let blocks = distanceFromHqInBlocks(pickupLocation) 
+      return blocks * 264;
+        
+}
+
+function distanceTravelledInFeet(start, destination) {
+
+    let start1 = parseInt(start)
+    let destination1 = parseInt(destination)
+
+        if (start1 > destination1) {
+            return (start1 - destination1) * 264
+        }
+        else return (destination1 - start1) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    
+   let distance = distanceTravelledInFeet(start, destination)
+   let fare
+
+   if (distance <= 400) {
+   
+       fare = 0
+       return fare
+       
+   }
+   else if (distance > 400, distance < 2000) {
+       fare = (distance-400) * .02
+       return fare
+   }
+
+   else if (distance > 2000, distance < 2500) {
+    fare = 25
+    return fare
+}
+    else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+}
